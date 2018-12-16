f19b58493ae689c0bbf055fe50a14a4fbe82437e
Kenneth Choi <kennethichoi@Kenneths-MacBook-Pro.local>
2018-12-16 13:21:56 -0800

    Main video details player, default search parameters, updated styling

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./VideoItem.css";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="item video-item">
+    <div
+      className="item video-item"
+      onClick={() => {
+        onVideoSelect(video);
+      }}
+    >
       <img
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
